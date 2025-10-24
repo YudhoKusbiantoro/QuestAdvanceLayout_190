ce4fa82693562794cdeb5c976dc7ea682e630639
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-10-24 10:17:50 +0700

    update menambahkan padding pada gambar

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -58,6 +58,7 @@ fun ActivityPertama(modifier: Modifier) {
                     contentDescription = null,
                     modifier = Modifier
                         .size(size = 100.dp)
+                        .padding(all = 5.dp)
                 )
                 Spacer(modifier = Modifier.width(width = 30.dp))
                 Column(){
