6d155def0e2885c0cb87dcc86bb91c9c5786bb04
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-10-24 09:55:26 +0700

    Membuat card dan menambahkan row untuk elemen

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -43,3 +43,11 @@ fun ActivityPertama(modifier: Modifier) {
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding( all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ){
