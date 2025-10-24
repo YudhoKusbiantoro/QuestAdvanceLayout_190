3bc3b319ac2b216d5c94af0ebe1c85662a53969b
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-10-24 09:53:19 +0700

    Menambahkan teks untuk menampilkan nama univ dan prodi

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -32,4 +32,13 @@ fun ActivityPertama(modifier: Modifier) {
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ) {
+        Text(
+            text = stringResource( id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 22.sp
+        )
